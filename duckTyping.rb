fef441b850089b52882f349f7ea63a1f5191caa4
d4dapo <d4dapo@gmail.com>
2015-07-29 08:19:52 +0000

    duck typing practice

---
+++ duckTyping.rb
@@ -0,0 +1,33 @@
+class Person
+  attr_reader :first_name, :last_name, :gender
+  def initialize first_name, last_name, gender
+    @first_name = first_name
+    @last_name = last-name 
+    @gender =gender
+  end
+  
+  def talk
+    "Hello"
+  end
+  
+end
+class Animal
+  def initialize name
+    @name =name
+  end
+  
+  def talk
+    "Woof, meow, roar! One of those"
+  end  
+end
+
+class Bug
+  def initialize type 
+    @type =type
+  end
+  def talk
+    "Do bugs even talk....?"
+  end
+end
+list=[]
+list.inject("suleiman", "goat", "ants").each {|item| puts item.talk}
