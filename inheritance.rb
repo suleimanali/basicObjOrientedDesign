d0936648a965e00d1b74773fc467d8de87b14ea0
d4dapo <d4dapo@gmail.com>
2015-07-29 07:10:36 +0000

    basic refresher inheritance

---
+++ inheritance.rb
@@ -0,0 +1,7 @@
+class Animal
+  attr_accessor :name, :type
+  def(name, type)
+    @name = name
+    @type = type
+  end
+end
---
+++ inheritance2.rb
@@ -0,0 +1,9 @@
+class Person
+  def initialize first_name, last_name, gender
+    @first_name = first_name
+    @last_name = last_name
+    @gender = gender    
+  end
+end
+
+person = Person.new("suleiman", "ali",89)
