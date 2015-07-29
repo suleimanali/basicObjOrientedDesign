0892d750302e795f2205487248215540f69fe6a5
d4dapo <d4dapo@gmail.com>
2015-07-29 07:28:52 +0000

    refactored Animal class and added other jargons

--- inheritance.rb
+++ inheritance.rb
@@ -1,7 +1,26 @@
 class Animal
   attr_accessor :name, :type
-  def(name, type)
+  def intialize name, type, legs_or_crawl
     @name = name
     @type = type
+    @legs_or_crawl =legs_or_crawl
   end
 end
+
+class Reptile < Animal
+  def initialize name,type, legs_or_crawl, water_land
+    super name, type,legs_or_crawl
+    @water_land = water_land
+  end  
+end
+
+class Mammals < Animal
+  def initialize name, type,legs_or_crawl,how_manny_legs
+    super name, type,legs_or_crawl
+    @how_manny_legs = how_manny_legs    
+  end 
+end
+
+reptile = Reptile.new("snake", "reptile", "crawl", "both")
+  
+  
