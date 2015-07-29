e23fd9b5e2b1b50ada638a90ef7d136cc9c0ca0a
d4dapo <d4dapo@gmail.com>
2015-07-29 08:02:18 +0000

    basic polymorphism taking place in line 45, 33 to update the present method in line 22

--- polymorphism.rb
+++ polymorphism.rb
@@ -1,27 +1,37 @@
 class Person
-  attr_reader :first_name, :last_name, :gender #used access the instance variable from outside the class
-  attr_writer :first_name #let's you overwrite the instance variable.
+  attr_reader :first_name, :last_name
   def initialize first_name, last_name, gender
     @first_name = first_name
     @last_name = last_name
     @gender = gender    
   end
 
+  def first_name
+    @first_name.capitalize!
+
+  end
+  def last_name
+    @last_name.capitalize!
+
+  end
+
   def full_name
     first_name + " "+ last_name
   end
   
-  private
-  def lifting_packages
-    "Can you lift more than 25lbs" unless gender == "F"  
-       
+  def present
+    "Hello, my name is #{full_name}, my gender is #[gender]."
   end
 end
 
 class Screencaster < Person
   def initialize first_name, last_name, gender, tools
-    super first_name, last_name, gender
+    super first_name,last_name,gender
     @tools = tools
+  end
+  
+  def present #update to represent something else than what is in the present method in line 22.
+    "Welcome to our family house Mr. #{last_name}."
   end
   
 end
@@ -32,8 +42,11 @@ class Student <Person
     @preffered_language = preffered_language
   end
   
+  def present #update to represent something else than what is in the present method in line 22.
+    "How is your first week in school Mrs. #{last_name}."
+  end
+  
 end
 person = Person.new("suleiman", "ali", "M")
 caster = Screencaster.new("suleiman", "ali", "M", ["cool", "mic"])
-caster.full_name
 student = Screencaster.new("suleiman", "ali", "M", ["cool", "mic"])
