2227bae1860785c6da40d46d256daa751dfb9a97
d4dapo <d4dapo@gmail.com>
2015-07-29 07:48:18 +0000

    encapsulation and some other jargon; great refresher

--- encapsulation.rb
+++ encapsulation.rb
@@ -1,27 +1,26 @@
 class Person
+  attr_reader :first_name, :last_name, :gender #used access the instance variable from outside the class
+  attr_writer :first_name #let's you overwrite the instance variable.
   def initialize first_name, last_name, gender
     @first_name = first_name
     @last_name = last_name
     @gender = gender    
   end
 
-  def first_name
-    @first_name.capitalize!
-
-  end
-  def last_name
-    @last_name.capitalize!
-
-  end
-
   def full_name
     first_name + " "+ last_name
+  end
+  
+  private
+  def zoo
+    "Ship to Africa" unless gender == "M"  
+       
   end
 end
 
 class Screencaster < Person
   def initialize first_name, last_name, gender, tools
-    super first_name,last_name,gender
+    super first_name, last_name, gender
     @tools = tools
   end
   
@@ -36,4 +35,5 @@ class Student <Person
 end
 person = Person.new("suleiman", "ali", "M")
 caster = Screencaster.new("suleiman", "ali", "M", ["cool", "mic"])
+caster.full_name
 student = Screencaster.new("suleiman", "ali", "M", ["cool", "mic"])
